49edf3ece33e9c5c1f5d5ed6356a5a624441ca68
DeadNord <83457266+DeadNord@users.noreply.github.com>
2022-08-17 10:40:20 +0200

    add c2c

--- src/balance/balance.service.ts
+++ src/balance/balance.service.ts
@@ -1,5 +1,6 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+import { log } from 'console';
 import { Model } from 'mongoose';
 import { CreateC2CTransDto } from './dto/create-c2c-trans.dto copy';
 import { CreateTransDto } from './dto/create-trans.dto';
@@ -38,25 +39,6 @@ export class BalanceService {
         ],
       });
 
-      // await this.balanceModel.findOneAndUpdate(
-      //   { id: createTransDto.id },
-      //   {
-      //     transactions: [
-      //       {
-      //         from: 'System',
-      //         to: createTransDto.id,
-      //         value: createTransDto.sum,
-      //         date: new Date(),
-      //         comment:
-      //           createTransDto.comment === ''
-      //             ? createTransDto.sum < 0
-      //               ? 'Outcome'
-      //               : 'Income'
-      //             : createTransDto.comment,
-      //       },
-      //     ],
-      //   },
-      // );
       return;
     }
 
@@ -91,7 +73,73 @@ export class BalanceService {
   }
 
   async createC2CTrans(createC2CTransDto: CreateC2CTransDto) {
-    return 'This action adds a new balance';
+    // const userFrom = await this.balanceModel.findOne({
+    //   id: createC2CTransDto.from,
+    // });
+
+    // if (!userFrom) {
+    //   throw new BadRequestException(`Invalid sender name`);
+    // }
+
+    // console.log('YY');
+
+    const user = await this.balanceModel.findOne({ id: createC2CTransDto.id });
+
+    if (!user) {
+      // console.log('YY');
+      if (createC2CTransDto.sum < 0) {
+        throw new BadRequestException(`Insufficient funds on the account`);
+      }
+      await this.balanceModel.create({
+        id: createC2CTransDto.id,
+        balance: createC2CTransDto.sum,
+        transactions: [
+          {
+            from: createC2CTransDto.from,
+            to: createC2CTransDto.id,
+            value: createC2CTransDto.sum,
+            date: new Date(),
+            comment:
+              createC2CTransDto.comment === ''
+                ? createC2CTransDto.sum < 0
+                  ? 'Outcome'
+                  : 'Income'
+                : createC2CTransDto.comment,
+          },
+        ],
+      });
+
+      return;
+    }
+
+    if (user.balance + createC2CTransDto.sum < 0) {
+      throw new BadRequestException(`Insufficient funds on the account`);
+    }
+
+    await this.balanceModel.findOneAndUpdate(
+      { id: createC2CTransDto.id },
+      {
+        balance: user.balance + createC2CTransDto.sum,
+        $push: {
+          transactions: [
+            {
+              from: createC2CTransDto.from,
+              to: createC2CTransDto.id,
+              value: createC2CTransDto.sum,
+              date: new Date(),
+              comment:
+                createC2CTransDto.comment === '' || null || undefined
+                  ? createC2CTransDto.sum < 0
+                    ? 'Outcome'
+                    : 'Income'
+                  : createC2CTransDto.comment,
+            },
+          ],
+        },
+      },
+    );
+
+    return;
   }
 
   async getBalance(id: string) {
